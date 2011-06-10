4532e797ff26947ee0f02f97d656c2ef9465cb10
Pavel Mitin <mitin.pavel@gmail.com>
2011-06-10 06:37:33 +0300

    Config files were removed from the future engine application.

--- current_user/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-CurrentUser::Application.config.secret_token = 'fdbdd218515f29c58d0b33f1addd80699e2481bc24a8414a7b182e8c3edbcd13a95bcb77f0cd766b3dcb1d7f56241069fac19db09956fba94f66c87e697fd685'
