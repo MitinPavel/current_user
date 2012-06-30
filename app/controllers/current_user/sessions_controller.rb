4a69fe8534cc368e65e66712f539f3a53b551de6
Pavel Mitin <mitin.pavel@gmail.com>
2012-07-01 00:29:18 +0300

    Add sorting by emails on the signin page.

--- app/controllers/current_user/sessions_controller.rb
+++ app/controllers/current_user/sessions_controller.rb
@@ -5,7 +5,7 @@ module CurrentUser
     before_filter :check_key
 
     def new
-      @users = ::User.all
+      @users = ::User.order 'email'
       @sign_in_url = key_sign_in_url ::CurrentUser::Config.instance.key
     end
 
