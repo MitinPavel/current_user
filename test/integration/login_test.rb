7daf24ad716bbee468e8c1f3dde4a02c6e388ed2
Pavel Mitin <mitin.pavel@gmail.com>
2012-06-30 16:31:13 +0300

    Start to implement the sign in page.

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -13,6 +13,7 @@ class LoginTest < ActionDispatch::IntegrationTest
     visit '/current_user/keys/12345/sign_in/new'
     assert_equal page.status_code, 200, 'Should respond with the OK http status code'
 
+    page.click_link_or_button 'Login as admin@my.app.com'
     page.click_link 'admin@my.app.com'
     assert page.has_content?('Welcome admin@my.app.com')
   end
